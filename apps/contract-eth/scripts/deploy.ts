fa7bcc20d62ffc7147d714e7693dfd60a7bc821b
PHCitizen <75726261+PHCitizen@users.noreply.github.com>
2025-07-29 11:52:25 +0000

    add resolver contract

--- apps/contract-eth/scripts/deploy.ts
+++ apps/contract-eth/scripts/deploy.ts
@@ -1,9 +1,11 @@
 import { network } from "hardhat";
 
 let WETH_ADDR = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+let LOP_ADDR: string | null = null;
 let FEE_TOKEN_ADDR: string | null = null;
 let ACCESS_TOKEN_ADDR: string | null = null;
-let LOP_ADDR: string | null = null;
+let ESCROW_FACTORY_ADDR: string | null = null;
+let RESOLVER_CONTRACT_ADDR: string | null = null;
 
 const { ethers } = await network.connect();
 
@@ -16,7 +18,8 @@ if (!chainId) {
   process.exit();
 }
 
-const [feeOwner, accessOwner, escrowFactoryOwner] = await ethers.getSigners();
+const [feeOwner, accessOwner, escrowFactoryOwner, resolverOwner] =
+  await ethers.getSigners();
 
 if (!LOP_ADDR) {
   const LOPFactory = await ethers.getContractFactory("LimitOrderProtocol");
@@ -46,16 +49,27 @@ if (!ACCESS_TOKEN_ADDR) {
   console.log("Access token deployed at:", ACCESS_TOKEN_ADDR);
 }
 
-// =================================
-// Escrow factory
-// =================================
-const EscrowFactory = await ethers.getContractFactory("EscrowFactory");
-const escrowFactory = await EscrowFactory.deploy(
-  LOP_ADDR,
-  FEE_TOKEN_ADDR,
-  ACCESS_TOKEN_ADDR,
-  escrowFactoryOwner.address,
-  1000n,
-  1000n
-);
-console.log("Escrow Factory deployed at:", await escrowFactory.getAddress());
+if (!ESCROW_FACTORY_ADDR) {
+  const EscrowFactory = await ethers.getContractFactory("EscrowFactory");
+  const escrowFactory = await EscrowFactory.deploy(
+    LOP_ADDR,
+    FEE_TOKEN_ADDR,
+    ACCESS_TOKEN_ADDR,
+    escrowFactoryOwner.address,
+    1000n,
+    1000n
+  );
+  ESCROW_FACTORY_ADDR = await escrowFactory.getAddress();
+  console.log("Escrow Factory deployed at:", ESCROW_FACTORY_ADDR);
+}
+
+if (!RESOLVER_CONTRACT_ADDR) {
+  const ResolverFactory = await ethers.getContractFactory("Resolver");
+  const resolverContract = await ResolverFactory.deploy(
+    ESCROW_FACTORY_ADDR,
+    LOP_ADDR,
+    resolverOwner.address
+  );
+  RESOLVER_CONTRACT_ADDR = await resolverContract.getAddress();
+  console.log("Escrow Factory deployed at:", RESOLVER_CONTRACT_ADDR);
+}
